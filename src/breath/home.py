f5e454cd913d978d27ea552b09a80fe67c00e40a
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-09-13 00:29:40 -0300

    Refatorado código de login

--- src/breath/home.py
+++ src/breath/home.py
@@ -4,99 +4,64 @@ import sqlite3
 import hashlib
 
 from breath.user.registrar import Registrar
+from breath.user.oauth import google_oauth_login
+from breath.user.db import *
 
 # Security
 #passlib,hashlib,bcrypt,scrypt
 
-def make_hashes(password):
-	return hashlib.sha256(str.encode(password)).hexdigest()
-
-def check_hashes(password,hashed_text):
-	if make_hashes(password) == hashed_text:
-		return hashed_text
-	return False
 # DB Management
 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
+
 # DB  Functions
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
-
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-	conn.commit()
-
-def login_user(username,password):
-	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-	data = c.fetchall()
-	return data
-
-
-def view_all_users():
-	c.execute('SELECT * FROM userstable')
-	data = c.fetchall()
-	return data
 
 
 
 def main():
-    try:
-        a = st.session_state.logado
-        del a 
-    except:
-        st.session_state.logado = False
+    if "logado" not in st.session_state:
+        st.session_state.logado = False 
+    
+    conn = sqlite3.connect('data.db')
+    bd = BD(conn)
 
-    menu = ["Home","Login","SignUp"]
-    choice = st.sidebar.selectbox("Menu",menu)
+    menu = ["Home", "Login", "Registrar"]
+    escolha = st.sidebar.selectbox("Menu", menu)
 
-    if choice == "Home":
+    if escolha == "Home":
         st.subheader("Home")
 
-    elif choice == "Login":
-        st.subheader("Login Section")
+    elif escolha == "Login":
+        st.subheader("Login")
+        st.sidebar.button("Login com Google", key="button_google_login")   
 
-        username = st.sidebar.text_input("User Name")
-        password = st.sidebar.text_input("Password",type='password')
-        if st.sidebar.button("Login"):
-            # if password == '12345':
-            create_usertable()
+        username = st.sidebar.text_input("Nome de usuário")
+        password = st.sidebar.text_input("Senha", type='password')
+
+        if st.sidebar.button("Login") or st.session_state.button_google_login:
+            if st.session_state.button_google_login:
+                google_oauth_login()
+
+                username = st.session_state.email
+                password = st.session_state.senha
+
             hashed_pswd = make_hashes(password)
 
-            result = login_user(username,check_hashes(password,hashed_pswd))
+            result = bd.login_user(username,check_hashes(password,hashed_pswd))
             if result:
-
-                st.success("Logged In as {}".format(username))
-
-                task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-                if task == "Add Post":
-                    st.subheader("Add Your Post")
-
-                elif task == "Analytics":
-                    st.subheader("Analytics")
-                elif task == "Profiles":
-                    st.subheader("User Profiles")
-                    user_result = view_all_users()
-                    clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-                    st.dataframe(clean_db)
+                st.success("Logado como {}".format(username))
             else:
-                st.warning("Incorrect Username/Password")
+                st.warning("Login incorreto")
 
-
-
-
-
-    elif choice == "SignUp":
+    elif escolha == "Registrar":
         Registrar()
 
-
         if st.session_state.logado:
-            create_usertable()
-            add_userdata(st.session_state.email,make_hashes(st.session_state.senha))
+            bd.add_userdata(st.session_state.email,make_hashes(st.session_state.senha))
             st.success("Você está logado")
-
 
 
 if __name__ == '__main__':
 	main()
+
+
