58d6ffaddf39d648a25fc88b7e8a91c89d98445c
Lucas-Otavio <l240106@dac.unicamp.br>
2021-11-04 22:35:51 -0300

    Update ui.py
    Refactoring Long Method Smell: method setupUi simplificado por meio dos methods createLabel e createPushButton

--- src/breath/ui.py
+++ src/breath/ui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import (QLabel, QLineEdit, QPushButton, QApplication, QMainWindow, QVBoxLayout, QDialog, QWidget)
 from PyQt6.QtGui import QFont
+from PyQt6.QtCore import (QRect, QCoreApplication, QMetaObject)
 
 class HomeWindow(object):
 	def __init__(self, bd, parent=None):
@@ -11,36 +12,41 @@ class HomeWindow(object):
 	def setupUi(self, Form):
 		Form.setObjectName("Form")
 		Form.resize(541, 566)
-		self.verticalLayoutWidget = QtWidgets.QWidget(Form)
-		self.verticalLayoutWidget.setGeometry(QtCore.QRect(70, 40, 391, 441))
+		self.verticalLayoutWidget = QWidget(Form)
+		self.verticalLayoutWidget.setGeometry(QRect(70, 40, 391, 441))
 		self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-		self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+		self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
 		self.verticalLayout.setContentsMargins(0, 0, 0, 0)
 		self.verticalLayout.setObjectName("verticalLayout")
-		self.title = QtWidgets.QLabel(self.verticalLayoutWidget)
-		font = QtGui.QFont()
+		self.title = QLabel(self.verticalLayoutWidget)
+		font = QFont()
 		font.setPointSize(20)
 		self.title.setFont(font)
 		self.title.setObjectName("title")
 		self.verticalLayout.addWidget(self.title)
-		self.lgn_text = QtWidgets.QLabel(self.verticalLayoutWidget)
-		self.lgn_text.setObjectName("lgn_text")
-		self.verticalLayout.addWidget(self.lgn_text)
-		self.lgn_btn = QtWidgets.QPushButton(self.verticalLayoutWidget)
-		self.lgn_btn.setObjectName("lgn_btn")
-		self.verticalLayout.addWidget(self.lgn_btn)
-		self.reg_text = QtWidgets.QLabel(self.verticalLayoutWidget)
-		self.reg_text.setObjectName("reg_text")
-		self.verticalLayout.addWidget(self.reg_text)
-		self.reg_btn = QtWidgets.QPushButton(self.verticalLayoutWidget)
-		self.reg_btn.setObjectName("reg_btn")
-		self.verticalLayout.addWidget(self.reg_btn)
+
+		self.reg_text = self.createLabel(self.verticalLayout, self.verticalLayoutWidget, "lgn_text")
+		self.lgn_btn = self.createPushBotton(self.verticalLayout, self.verticalLayoutWidget, "lgn_btn")
+		self.reg_text = self.createLabel(self.verticalLayout, self.verticalLayoutWidget, "reg_text")
+		self.reg_btn = self.createPushBotton(self.verticalLayout, self.verticalLayoutWidget, "reg_btn")
 
 		self.retranslateUi(Form)
-		QtCore.QMetaObject.connectSlotsByName(Form)
+		QMetaObject.connectSlotsByName(Form)
+
+	def createLabel(verticalLayout:QVBoxLayout, verticalLayoutWidget:QWidget, label:str) -> QLabel:
+		label:QLabel = QLabel(verticalLayoutWidget)
+		label.setObjectName(label)
+		verticalLayout.addWidget(label)
+		return label
+
+	def createPushBotton(verticalLayout:QVBoxLayout, verticalLayoutWidget:QWidget, label:str) -> QPushButton:
+		btn = QPushButton(verticalLayoutWidget)
+		btn.setObjectName(label)
+		verticalLayout.addWidget(btn)
+		return btn
 
 	def retranslateUi(self, Form):
-		_translate = QtCore.QCoreApplication.translate
+		_translate = QCoreApplication.translate
 		Form.setWindowTitle(_translate("Form", "Form"))
 		self.title.setText(_translate("Form", "  Bem vindo ao Sistema Breath"))
 		self.lgn_text.setText(_translate("Form", "    Por favor faça seu login para que possamos continuar"))
