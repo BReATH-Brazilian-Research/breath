2342b0e6ff3ba276a8a9aee794349d15a703119f
J <joao.bonucci@conpec.com.br>
2021-11-22 20:05:10 -0300

    test function declaration for bdAccessPoint

---
+++ test/bda_test.py
@@ -0,0 +1,38 @@
+import unittest
+import sys
+
+sys.path.append("..\src")
+
+from breath.session_manager import ProcessSessionManager
+
+class BDTest(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	
+	def setUp(cls):
+		pass
+
+	
+	def tearDown(cls):
+		pass
+
+	def test_limit_date(self):
+		pass
+
+	def test_limit_lon_lat(self):
+		pass
+
+	def test_valid_state_city(self):
+		pass
+
+
+
+if __name__ == '__main__':
+	unittest.main()
