a06c6e53f82ffcb146ac6c52b401afc53da47a41
J <joao.bonucci@conpec.com.br>
2021-11-22 22:48:55 -0300

    bd service tests

--- src/breath/session_manager/session_manager.py
+++ src/breath/session_manager/session_manager.py
@@ -12,9 +12,12 @@ class ProcessSessionManager:
     def __init__(self):
         self._queue = ProcessQueue()
         self._global_response_queue = ProcessQueue()
+        self._sm_response_queue = ProcessQueue()
 
         self._service_constructor = ProcessServiceConstructor()
         self._request_manager = RequestManager(self._queue, self._global_response_queue, self)
+
+        self._request_manager.register_service("SESSION_MANAGER", None, self._sm_response_queue)
 
     def create_service(self, service_name:str):
         request_queue, response_queue = self._service_constructor.create_service(service_name, self._queue, self._global_response_queue)
@@ -26,3 +29,16 @@ class ProcessSessionManager:
 
     def run(self):
         self._request_manager.process_request()
+
+    def send_request(self, service_name, operation_name, request_info=None, wait_for_response=True):
+        request = Request(service_name, operation_name, "SESSION_MANAGER", request_info, wait_for_response)
+        
+        self._request_manager.queue.insert(request)
+
+        while self._sm_response_queue.empty():
+            time.sleep(1E-3)
+
+        if not wait_for_response:
+            return None
+
+        return self._sm_response_queue.get()
