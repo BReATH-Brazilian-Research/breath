aa48554f641527adb87c49d6ec37c9b22f2d0039
Lucas-Otavio <l240106@dac.unicamp.br>
2021-12-08 20:15:16 -0300

    Squashed commit of the following:
    commit 300adf3814440fa3109c7fdcfe0b3ac3a919b6c6
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 20:13:50 2021 -0300
    
        Update climate_request.py
    
    commit a10b0a598c4001759edfe01faacdd15044dcab1c
    Merge: 7a5843c a8ad2b0
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 20:00:33 2021 -0300
    
        Merge branch 'feature/climate_checker' of https://gitlab.com/EltonCN/breath-brazilian-research-of-atmosphere-towards-health into feature/climate_checker
    
    commit a8ad2b09a6659c79ac319c83fec8008b2602c0f2
    Author: EltonCN <43186596+EltonCN@users.noreply.github.com>
    Date:   Wed Dec 8 19:59:52 2021 -0300
    
        Implementado exit
    
    commit 7a5843c8f6dff345296c55ac19b397a976e0a544
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 19:53:04 2021 -0300
    
        Update console_application.py
    
    commit a488bc3fc9dc95440bdef1147412eee8cd8d0fde
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 19:51:10 2021 -0300
    
        Update console_application.py
    
        Fixed typo
    
    commit b441eb26c57cb8a228a5e19f31f10d19805cc36a
    Author: EltonCN <43186596+EltonCN@users.noreply.github.com>
    Date:   Wed Dec 8 19:47:21 2021 -0300
    
        Adicionado gitignore
    
    commit 0d9dfbe3fdf918bea30307a1c1879ed9f59bee8a
    Merge: 1208d6c 5095ec2
    Author: EltonCN <43186596+EltonCN@users.noreply.github.com>
    Date:   Wed Dec 8 19:45:46 2021 -0300
    
        Merge branch 'feature/climate_checker' of https://gitlab.com/breath_unicamp/breath-brazilian-research-of-atmosphere-towards-health into feature/climate_checker
    
    commit 1208d6c424433bdc991e54aaf325aa1bf575acc5
    Author: EltonCN <43186596+EltonCN@users.noreply.github.com>
    Date:   Wed Dec 8 19:45:29 2021 -0300
    
        Corrigido problema com stdin no ConsoleApplication
    
    commit 5095ec2d672505d8bf67f9241e362ce131498b35
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 19:41:44 2021 -0300
    
        Small Fixes
    
    commit 94651e93f27d4643bcc35b294154e3083b7c2980
    Merge: fbae8c3 c75e3b6
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 19:35:58 2021 -0300
    
        Merge branch 'feature/climate_checker' of https://gitlab.com/EltonCN/breath-brazilian-research-of-atmosphere-towards-health into feature/climate_checker
    
    commit fbae8c30ad4088bf7e1680bf834d863accae47fa
    Author: Lucas-Otavio <l240106@dac.unicamp.br>
    Date:   Wed Dec 8 19:35:48 2021 -0300
    
        Update .gitignore
    
    commit c75e3b6c45a917ff581ff48b48f1f8c707180d8c
    Author: J <joao.bonucci@conpec.com.br>
    Date:   Wed Dec 8 19:17:56 2021 -0300
    
        finalizacao de obtencao dos dados de clima
    
    commit db31b8918ad1dc1567c7ef86eaed6dbd83603f9d
    Author: J <joao.bonucci@conpec.com.br>
    Date:   Wed Dec 8 16:37:16 2021 -0300
    
        implementando teste de requisicao de API

---
+++ src/breath_main/console_application/climate_request.py
@@ -0,0 +1,75 @@
+import requests, json, pdb, datetime
+
+def get_clima():
+
+	url = "https://community-open-weather-map.p.rapidapi.com/climate/month"
+
+	city = input("Digite uma cidade omitindo os acentos e ç:")
+
+	querystring = {"q":city}
+
+	headers = {
+	    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+	    'x-rapidapi-key': "24117470c7msh69c4f7f119cb290p144f8djsnfa7d413f68a3"
+	    }
+
+	response = requests.request("GET", url, headers=headers, params=querystring)
+
+	clima_data = None
+	if response.status_code == 200:
+		clima_data = response.json()
+
+	for item in clima_data['list']:
+		prev = datetime.datetime.fromtimestamp(int(item['dt']))
+		now = datetime.datetime.now()
+
+		if prev > now:
+			print(f"Para {city}, na hora:", prev)
+			print("Umidade",round(item['humidity']),"%")
+			print("Pressão",round(item['pressure']),"Pa")
+			print("Máxima",round(item['temp']['average_max']-273,1),"ºC")
+			print("Mínima",round(item['temp']['average_min']-273,1),"ºC")
+			break
+
+	if clima_data is not None:
+		# Airmine AQI endpoint
+		endpoint = "https://pm251.p.rapidapi.com/aqi"
+		# Coordinates of Berlin
+		coordinates = {
+			"lat": clima_data['city']['coord']['lat'],
+			"lon": clima_data['city']['coord']['lon']
+		}
+		# Custom headers
+		headers = {
+			'x-rapidapi-host': "pm251.p.rapidapi.com/aqi",
+			'x-rapidapi-key': "24117470c7msh69c4f7f119cb290p144f8djsnfa7d413f68a3"
+		}
+
+		# Execute a GET request
+		response = requests.request(
+			  "GET", endpoint,
+			  headers = headers, params = coordinates
+		)
+
+		air_data = None
+		if response.status_code == 200:
+			air_data = response.json()
+
+		if air_data is not None:
+			n = len(air_data['forecasts'])	
+			for item in air_data['forecasts']:
+				date = item['date']
+				d1 = datetime.datetime.strptime(date,"%Y-%m-%dT%H:%M:%S")
+				d2 = datetime.datetime.now()
+				pm25 = item['values'][0]['value']
+				if (d1 > d2):
+					print("%s | %d pm25" % (date, pm25))
+					break
+				else:
+					print(date, 'futuro')	
+			#remaining = int(response.headers['x-ratelimit-requests-remaining'])
+	 		#print("You have %d requests left" % remaining)
+		else:
+			print("Nao pudemos achar os dados de qualidade do ar para sua cidade")
+	else:
+		print("Nao possuimos informacoes da sua cidade")
--- src/breath_main/console_application/console_application.py
+++ src/breath_main/console_application/console_application.py
@@ -3,28 +3,48 @@ from breath_api_interface.proxy import ServiceProxy
 from breath_api_interface.queue import Queue
 from breath_api_interface.service_interface import Service
 from breath_api_interface.request import Request, Response
+from .climate_request import get_clima
+import pdb
 
 import sys
 
 class ConsoleApplication(Service):
-    def __init__(self, proxy:ServiceProxy, request_queue:Queue, global_response_queue:Queue):
-        '''ConsoleApplication constructor.
-        '''
-        super().__init__(proxy, request_queue, global_response_queue, "ConsoleApplication")
-        self._configured = False
+	def __init__(self, proxy:ServiceProxy, request_queue:Queue, global_response_queue:Queue):
+		'''ConsoleApplication constructor.
+		'''
+		super().__init__(proxy, request_queue, global_response_queue, "ConsoleApplication")
+
+		sys.stdin = open(0)
+		self._configured = False
 
 
-    def run(self) -> None:
+	def run(self) -> bool:
 
-        print("Escolha uma opção:")
-        print("1 - Construir base de dados")
-        
-        opcao = 1
+		print("Escolha uma opção:")
+		print("1 - Construir base de dados")
+		print("2 - Dados climáticos de qualidade do ar de uma cidade")
+		print("3 - Histórico de doenças respiratórias da cidade")
+		print("4 - Probablidade de doenças agora")
+		print("5 - Registrar meus sintomas")
+		print("6 - Ver histórico de sintomas")
+		print("7 - Sair da aplicação")
 
-        if opcao == 1:
-            response = self._send_request(service_name="DataWorkflow", operation_name="load_open_sus_data")
+		
+		opcao = sys.stdin.readline()
+		opcao = int(opcao)
 
-        if response.sucess:
-            print("Operação realizada com sucesso!")
-        else:
-            print("Erro: ", response.response_data["message"])
+		if opcao == 2:
+			get_clima()
+		elif opcao == 7:
+			self._send_request("SESSION_MANAGER", "exit")
+		return True
+
+		#if opcao == 1:
+		#    response = self._send_request(service_name="DataWorkflow", operation_name="load_open_sus_data")
+#
+		#if response.sucess:
+		#    print("Operação realizada com sucesso!")
+		#else:
+		#    print("Erro: ", response.response_data["message"])
+
+	
