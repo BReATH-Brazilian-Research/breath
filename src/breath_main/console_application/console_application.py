84c2e82d208ee8e1b91a06340923082a33c32049
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-12-08 21:22:26 -0300

    Adicionado operação para iniciar banco de dados

--- src/breath_main/console_application/console_application.py
+++ src/breath_main/console_application/console_application.py
@@ -18,33 +18,34 @@ class ConsoleApplication(Service):
 		self._configured = False
 
 
-	def run(self) -> bool:
+	def run(self):
 
 		print("Escolha uma opção:")
 		print("1 - Construir base de dados")
-		print("2 - Dados climáticos de qualidade do ar de uma cidade")
-		print("3 - Histórico de doenças respiratórias da cidade")
-		print("4 - Probablidade de doenças agora")
-		print("5 - Registrar meus sintomas")
-		print("6 - Ver histórico de sintomas")
+		print("2 - Dados climáticos atuais de qualidade do ar de uma cidade")
+
+		if self._configured:
+			print("3 - Histórico de doenças respiratórias da cidade")
+			print("4 - Probablidade de doenças agora")
+			print("5 - Registrar meus sintomas")
+			print("6 - Ver histórico de sintomas")
 		print("7 - Sair da aplicação")
 
 		
 		opcao = sys.stdin.readline()
 		opcao = int(opcao)
 
-		if opcao == 2:
+		if opcao == 1:
+			response = self._send_request("DataWorkflow", "run_workflow", request_info={"workflow_name":"BDDownloader"})
+
+			if response.sucess:
+				self._configured = True
+			else:
+				print("Problema ao iniciar banco de dados: "+response.response_data["message"])
+		
+		elif opcao == 2:
 			get_clima()
 		elif opcao == 7:
 			self._send_request("SESSION_MANAGER", "exit")
-		return True
-
-		#if opcao == 1:
-		#    response = self._send_request(service_name="DataWorkflow", operation_name="load_open_sus_data")
-#
-		#if response.sucess:
-		#    print("Operação realizada com sucesso!")
-		#else:
-		#    print("Erro: ", response.response_data["message"])
-
-	
+		elif self._configured:
+			...
